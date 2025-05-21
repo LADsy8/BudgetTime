5af250dd7920d5e99957d6cfe637dfd9c803e90d
LADsy8 <dube.arthur@gmail.com>
2025-05-21 16:03:14 -0400

    0.1.2: ...

--- src/Controller/main.java
+++ src/Controller/main.java
@@ -2,20 +2,11 @@ package Controller;
 
 import java.awt.EventQueue;
 
-import Model.BudgetApp;
 import View.UI_Interface;
 
 public class main {
 
 	public static void main(String[] args) {
-		BudgetApp budgetApp = new BudgetApp();
-		budgetApp.addTransaction("Salary", 3000.0);
-		budgetApp.addTransaction("Rent", -1000.0);
-		budgetApp.addTransaction("Utilities", -200.0);
-		budgetApp.addTransaction("Groceries", -300.0);
-		System.out.println("Total Income: $" + budgetApp.calculateTotalIncome());
-		System.out.println("Total Expenses: $" + budgetApp.calculateTotalExpenses());
-		System.out.println("Budget Balance: $" + budgetApp.calculateBudgetBalance());
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
--- src/View/UI_Interface.java
+++ src/View/UI_Interface.java
@@ -1,43 +1,37 @@
 package View;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+
+import Model.BudgetApp;
 
 public class UI_Interface extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel pnlControl;
-	private final JLabel lblInfo = new JLabel("123456789");
 
 	/**
 	 * Create the frame.
 	 */
 	public UI_Interface() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 735, 420);
-		pnlControl = new JPanel();
-		pnlControl.setBackground(new Color(0, 128, 255));
-		pnlControl.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(pnlControl);
-
-		JButton btnRun = new JButton("Run");
-		btnRun.addActionListener(new ActionListener() {
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(284, 135, 89, 23);
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblInfo.setText("Comp310 rocks!");
 			}
 		});
-		pnlControl.add(btnRun);
-		lblInfo.setFont(new Font("Times New Roman", Font.PLAIN, 13));
-		pnlControl.add(lblInfo);
+		getContentPane().setLayout(null);
+		getContentPane().add(btnNewButton);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 714, 394);
+		getContentPane().add(panel);
+		BudgetApp budgetApp = new BudgetApp();
+
 	}
 
 }
