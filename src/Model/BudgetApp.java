4400508b39bc2323f9bf041afb9538bae40c95a9
LADsy8 <dube.arthur@gmail.com>
2025-05-22 14:51:34 -0400

    0.1.3: Ajout d'une interface Mieux
    -plus de label
    -bouton pour achat et ajout au compte
    -label pour la balance total
    -ajout d'une liste Box sans savoir ce que sa fais (pour afficher la liste des transactions effectuer

--- src/Model/BudgetApp.java
+++ src/Model/BudgetApp.java
@@ -4,30 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BudgetApp {
+
 	private List<Transaction> transactions = new ArrayList<>();
-    public void addTransaction(String description, double amount) {
-        Transaction transaction = new Transaction(description, amount);
-        transactions.add(transaction);
-    }
-    public double calculateTotalIncome() {
-        double totalIncome = 0;
-        for (Transaction transaction : transactions) {
-            if (transaction.getAmount() > 0) {
-                totalIncome += transaction.getAmount();
-            }
-        }
-        return totalIncome;
-    }
-    public double calculateTotalExpenses() {
-        double totalExpenses = 0;
-        for (Transaction transaction : transactions) {
-            if (transaction.getAmount() < 0) {
-                totalExpenses += Math.abs(transaction.getAmount());
-            }
-        }
-        return totalExpenses;
-    }
-    public double calculateBudgetBalance() {
-        return calculateTotalIncome() - calculateTotalExpenses();
-    }
+
+	public void addTransaction(String description, double amount) {
+		Transaction transaction = new Transaction(description, amount);
+		transactions.add(transaction);
+	}
+
+	public double calculateTotalIncome() {
+		double totalIncome = 0;
+		for (Transaction transaction : transactions) {
+			if (transaction.getAmount() > 0) {
+				totalIncome += transaction.getAmount();
+			}
+		}
+		return totalIncome;
+	}
+
+	public double calculateTotalExpenses() {
+		double totalExpenses = 0;
+		for (Transaction transaction : transactions) {
+			if (transaction.getAmount() < 0) {
+				totalExpenses += Math.abs(transaction.getAmount());
+			}
+		}
+		return totalExpenses;
+	}
+
+	public double calculateBudgetBalance() {
+		return calculateTotalIncome() - calculateTotalExpenses();
+	}
 }
--- src/View/UI_Interface.java
+++ src/View/UI_Interface.java
@@ -1,37 +1,83 @@
 package View;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-
-import Model.BudgetApp;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class UI_Interface extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private JPanel contentPanel;
+	private JTextField txtFieldAchat;
+	private JTextField txtEntrerLeMontant;
 
 	/**
 	 * Create the frame.
 	 */
 	public UI_Interface() {
+		setTitle("Suiveur de Budget");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 739, 472);
+		contentPanel = new JPanel();
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPanel);
+		contentPanel.setLayout(null);
+		JButton btnAchat = new JButton("Achat");
+		btnAchat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(284, 135, 89, 23);
-		btnNewButton.addActionListener(new ActionListener() {
+			}
+		});
+		btnAchat.setBounds(144, 63, 89, 23);
+		contentPanel.add(btnAchat);
+
+		JButton btnAjout = new JButton("Ajout");
+		btnAjout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		getContentPane().setLayout(null);
-		getContentPane().add(btnNewButton);
+		btnAjout.setBounds(486, 63, 89, 23);
+		contentPanel.add(btnAjout);
 
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 714, 394);
-		getContentPane().add(panel);
-		BudgetApp budgetApp = new BudgetApp();
+		txtFieldAchat = new JTextField();
+		txtFieldAchat.setText("Entrer le cout de votre achat");
+		txtFieldAchat.setBounds(98, 97, 186, 20);
+		contentPanel.add(txtFieldAchat);
+		txtFieldAchat.setColumns(10);
+
+		txtEntrerLeMontant = new JTextField();
+		txtEntrerLeMontant.setText("Entrer le montant de votre ajout");
+		txtEntrerLeMontant.setColumns(10);
+		txtEntrerLeMontant.setBounds(435, 97, 186, 20);
+		contentPanel.add(txtEntrerLeMontant);
+
+		JLabel lblNewLabel = new JLabel("Application De Budget");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(281, 11, 150, 31);
+		contentPanel.add(lblNewLabel);
+
+		JList<?> lstAchat = new JList();
+		lstAchat.setBounds(98, 143, 186, 136);
+		contentPanel.add(lstAchat);
+
+		JList<?> lstAjout = new JList();
+		lstAjout.setBounds(435, 143, 186, 136);
+		contentPanel.add(lstAjout);
+
+		JLabel lblBalance = new JLabel("TotalBalance :");
+		lblBalance.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblBalance.setBounds(281, 316, 150, 20);
+		contentPanel.add(lblBalance);
 
 	}
-
 }
